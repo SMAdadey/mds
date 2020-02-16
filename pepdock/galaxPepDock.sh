d18e60f0199c20a9abb050183b0fd6831fcbd852
esohkevin <esohkevin4@gmail.com>
2020-02-16 14:38:02 +0200

    add_files

--- pepdock/galaxPepDock.sh
+++ pepdock/galaxPepDock.sh
@@ -3,20 +3,21 @@
 function usage() {
         printf "Usage: %s [-j job_type] [-p pdb_file] [-n project_name]\n" $(basename $0)
         echo """
-                Galaxy Model Refinement
+                Galaxy Protein-Peptide Docking
 
                 -j,--jtype     [clus/inte]     :Submit job on cluster [clus] or run interatively [inte]
-                -p,--pdb       <str>           :PDB file to refine
+                -p,--pdb       <str>           :PDB protein stucture
+                -s,--seq       <str>           :Peptide sequnce to dock in FASTA format
                 -n,--name      <str>           :Name of project
                 -h,--help      		       :Print this message
         """
 }
 
-if [[ $# != 6 ]]; then
+if [[ $# != 8 ]]; then
    usage
 
 else
-   prog=`getopt -o "hj:p:n:" --long "help,jtype:,pdb:,name:" -- "$@"`
+   prog=`getopt -o "hj:p:s:n:" --long "help,jtype:,pdb:,seq:,name:" -- "$@"`
    
    if [ $? != 0 ]; then 
       echo "An error occurred! Terminating..." 1>&2; 
@@ -29,6 +30,7 @@ else
        case "$1" in
           -j|--jtype) jtp="$2"; shift 2;;
           -p|--pdb) inpdb="$2"; shift 2;;
+          -s|--seq) seq="$2"; shift 2;;
           -n|--name) pname="$2"; shift 2;;
           -h|--help) shift; usage; 1>&2; exit 1 ;;
           --) shift; break ;;
@@ -40,15 +42,15 @@ else
    echo "Project name: $pname"
    
    if [[ $jtp == "inte" ]]; then
-      cd /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/modrefine/
-      export GALAXY_HOME=/mnt/lustre/users/kesoh/bioTools/GalaxyRefine      
+      cd /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/pepdock/
+      export GALAXY_HOME=/mnt/lustre/users/kesoh/bioTools/GalaxyPepDock 
       echo "==========================================="
       echo "PDB file: $inpdb"
-      echo "CMD: GalaxyRefine -p $inpdb -t $pname -o 10"
+      echo "CMD: GalaxyPepDock -t $pname -p $inpdb -s $seq"
       echo "==========================================="
       sleep 1
       echo "Running..."
-      GalaxyRefine -p $inpdb -t $pname -o 10
+      GalaxyPepDock.ubuntu1604 -p $inpdb -t $pname -s $seq
    
    elif [[ $jtp == "clus" ]]; then
       echo """#!/usr/bin/env bash
@@ -56,18 +58,17 @@ else
 #PBS -l walltime=48:00:00
 #PBS -q smp
 #PBS -P CBBI1243
-#PBS -o /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/modrefine/stdout.txt
-#PBS -e /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/modrefine/stderr.txt
+#PBS -o /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/pepdock/stdout.txt
+#PBS -e /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/pepdock/stderr.txt
 #PBS -N GalRefine
 #PBS -M kevin.esoh@students.jkuat.ac.ke
 #PBS -m b
 
-cd /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/modrefine/
-export GALAXY_HOME=/mnt/lustre/users/kesoh/bioTools/GalaxyRefine
+cd /mnt/lustre/groups/CBBI1243/KEVIN/git/mds/pepdock/
 
-GalaxyRefine -p $inpdb -t $pname -o 10
+set GALAXY_HOME=/mnt/lustre/users/kesoh/bioTools/GalaxyPepDock
+GalaxyPepDock.ubuntu1604 -p $inpdb -t $pname -s $seq
 
-#bash galaxRefine.sh -n ocrlWt -p ../OCRL.B99990019.pdb
 """ > ${pname}.qsub
 
    echo """
