c0f4c3750ac25eb6cd7e205d43ea7066576a961e
esohkevin <esohkevin4@gmail.com>
2020-04-20 10:03:14 +0200

    update_prep.pdb.sh

--- prep.pdb.sh
+++ prep.pdb.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-for i in $(cat covid19.acc); do echo "-c -d \'load ${i/_*/.pdb}; indicate chain ${i/*_/}; save ${i/_/_chain}.pdb\'"; done | parallel echo pymol {} | xargs -I input -P10 sh -c "input"
+for i in $(cat covid19.acc); do 
+  if [[ -f "${i/_*/.pdb}" ]]; then
+    mkdir -p raw_pdbs; 
+    mv ${i/_*/.pdb} raw_pdbs/;
+  fi
+    echo "-c -d \'load raw_pdbs/${i/_*/.pdb}; indicate chain ${i/*_/}; save ${i/_*/}.pdb\'"; 
+done | \
+     parallel echo pymol {} | xargs -I input -P11 sh -c "input"
