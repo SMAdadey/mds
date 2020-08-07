80c15bec0df11569d19dcabea61af4f6c1d3b290
esohkevin <esohkevin4@gmail.com>
2020-08-07 03:18:25 +0200

    update

--- mds_plot.R
+++ mds_plot.R
@@ -96,21 +96,23 @@ ansis_plot <- function(rmsda=first_rmsd_file,
    par(mfrow=c(2,1))
       plot(ra$time, ra$rmsd, 
            main="RMSD",
+           col="black"
       	   type="l",
-           ylim=c(0.1, 0.6),
+           ylim=c(0.15, 0.5),
       	   xlab="Time (ns)",
       	   ylab="RMSD (nm)")
       lines(rb$time, rb$rmsd,col="red")
-      legend("bottomright", legend=c("Wild Type","Mutant"), col=c("red","black"),pch="-")
+      legend("bottomright", legend=c("Wild Type","Mutant"), col=c("black","red"),pch="-")
 
       plot(ga$t_ns, ga$Rt,
            main="Radius of Gyration",
            xlab="Time (ns)",
            ylab="Rg (nm)",
-	   ylim=c(1.5, 2.5),
+	   ylim=c(1.7, 2.3),
+	   col="black"
 	   type="l")
       lines(gb$t_ns, gb$Rt,col="red")
-      legend("bottomright", legend=c("Wild Type","Mutant"), col=c("red","black"),pch="-")   
+      legend("bottomright", legend=c("Wild Type","Mutant"), col=c("black","red"),pch="-")   
 dev.off()
 }
 
